412cee958694ce7ae3a43772f42acb4dc81bed49
supermarioknight <hunter.dev.us@gmail.com>
2017-11-11 01:17:20 -0500

    resolve conflicts

--- frontend/containers/AppContainer.js
+++ frontend/containers/AppContainer.js
@@ -1,18 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-<<<<<<< HEAD
 import Title from '../components/Title';
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-
-const AppContainer = ({ name }) => {
-    return (
-        <div>
-            <NavBar />
-            <Footer />
-        </div>
-=======
 import Routes from './Routes';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -21,7 +12,6 @@ const AppContainer = ({ name }) => {
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
->>>>>>> 67eca5be665a79b4057abb41b35c6390230c9310
     );
 };
 
